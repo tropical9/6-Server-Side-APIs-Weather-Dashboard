f1c6bb213b9a6f3dc655adb37bb3150c850c41d9
tropical9 <113566952+tropical9@users.noreply.github.com>
2022-11-29 23:34:16 -0800

    bootstrap

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var APIKey = "6cfcd046bcdbf5f166a9d037293881fc";
+
+var city; 
+
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//q is query parameter, whre we can add user input to specify the data that we want to request
+
+fetch(queryURL)
